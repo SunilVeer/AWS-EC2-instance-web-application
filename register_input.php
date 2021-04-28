d6527cd3e04f8ae22cefb580709ffd48bff56154
SunilVeer <62995033+SunilVeer@users.noreply.github.com>
2021-04-28 12:02:17 +0530

    Update register_input.php

--- register_input.php
+++ register_input.php
@@ -11,7 +11,7 @@
  * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
-*/
+ */
 
 require 'vendor/autoload.php';
 
@@ -20,17 +20,14 @@ date_default_timezone_set('UTC');
 use Aws\DynamoDb\Exception\DynamoDbException;
 use Aws\DynamoDb\Marshaler;
 
-$sdk = new Aws\Sdk([
-    'region'   => 'us-east-1',
-    'version'  => 'latest'
-]);
+$sdk = new Aws\Sdk(['region' => 'us-east-1', 'version' => 'latest']);
 
 $dynamodb = $sdk->createDynamoDb();
 $marshaler = new Marshaler();
 
-$tableName = 'user'; 
+$tableName = 'user';
 
-$email = $_POST['email']; 
+$email = $_POST['email'];
 
 //check if email is already present in database
 $tableName = 'user';
@@ -39,42 +36,37 @@ $key = $marshaler->marshalJson('{
         "email": "' . $email . '"
 }');
 
-    $params = [
-            'TableName' => $tableName,
-            'Key' => $key
-        ];
-    $result = $dynamodb->getItem($params);
-	if($result['Item']!=null) {
-		foreach ($result as $r) {	
-		$output =  $marshaler->unmarshalItem($r);
-		if ($output['email'] == $email){
-			$email_error = "The Email ID already exists";
-                	header("location: register.php?email_error=$email_error"); 
-		}
-		}
-	}
+$params = ['TableName' => $tableName, 'Key' => $key];
+$result = $dynamodb->getItem($params);
+if ($result['Item'] != null)
+{
+    foreach ($result as $r)
+    {
+        $output = $marshaler->unmarshalItem($r);
+        if ($output['email'] == $email)
+        {
+            $email_error = "The Email ID already exists";
+            header("location: register.php?email_error=$email_error");
+        }
+    }
+}
 
 //email is not already present
-    $password = $_POST['password'];
-    $username = $_POST['username'];
-    $json = json_encode([
-        'email' => $email,
-        'password' => $password,
-        'username' => $username
-    ]);
+$password = $_POST['password'];
+$username = $_POST['username'];
+$json = json_encode(['email' => $email, 'password' => $password, 'username' => $username]);
 
-    $params = [
-        'TableName' => $tableName,
-        'Item' => $marshaler->marshalJson($json)
-];
+$params = ['TableName' => $tableName, 'Item' => $marshaler->marshalJson($json) ];
 
-        try {
-                $result = $dynamodb->putItem($params);
-                header("location: index.php");
-            } catch (DynamoDbException $e) {
-                echo "Unable to add user:\n";
-                echo $e->getMessage() . "\n";
-                }
-
+try
+{
+    $result = $dynamodb->putItem($params);
+    header("location: index.php");
+}
+catch(DynamoDbException $e)
+{
+    echo "Unable to add user:\n";
+    echo $e->getMessage() . "\n";
+}
 
 ?>
