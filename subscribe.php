8afa37ba1dc4e0ca343358b0a73671affec4c0d3
SunilVeer <62995033+SunilVeer@users.noreply.github.com>
2021-04-28 12:02:50 +0530

    Update subscribe.php

--- subscribe.php
+++ subscribe.php
@@ -4,31 +4,25 @@ require 'vendor/autoload.php';
 date_default_timezone_set('UTC');
 use Aws\DynamoDb\Exception\DynamoDbException;
 use Aws\DynamoDb\Marshaler;
-$sdk = new Aws\Sdk([
-    'region' => 'us-east-1',
-    'version' => 'latest'
-]);
+$sdk = new Aws\Sdk(['region' => 'us-east-1', 'version' => 'latest']);
 $dynamodb = $sdk->createDynamoDb();
 $marshaler = new Marshaler();
 
 $tableName = 'subscription';
 
-$json = json_encode([
-        'email' => $_GET['email'],
-        'title' => $_GET['title']
-    ]);
+$json = json_encode(['email' => $_GET['email'], 'title' => $_GET['title']]);
 
-    $params = [
-        'TableName' => $tableName,
-        'Item' => $marshaler->marshalJson($json)
-];
-		
-try {
-	    $result = $dynamodb->putItem($params);
-		header("location: main_page.php");
+$params = ['TableName' => $tableName, 'Item' => $marshaler->marshalJson($json) ];
 
-    } catch (DynamoDbException $e) {
-	echo "Unable to get item:\n";
-	echo $e->getMessage() . "\n";
+try
+{
+    $result = $dynamodb->putItem($params);
+    header("location: main_page.php");
+
+}
+catch(DynamoDbException $e)
+{
+    echo "Unable to get item:\n";
+    echo $e->getMessage() . "\n";
 }
 ?>
